639ef455c0612bc0db1fc50869e0b7e784c2f8e2
Samuel FERREIRA (valtech) <samuel.ferreira@valtech.com>
2023-01-20 15:41:05 -0300

    Margin and padding ajust

--- src/components/CalendarIframe.vue
+++ src/components/CalendarIframe.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
   <div class="iframe-container">
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo&mode=WEEK&showPrint=0&showTabs=1&showDate=1&showNav=1&showCalendars=0&showTz=1&title=IASD%20Parada%20Inglesa&src=MmYwMTBjNjUyOTExNDgzZGJmZDQ0MmM3MzIxOTAzMTNmZDQ4YzcyNzcxMTRmNzFkNDIyYzEyNDFiODg0YTcwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA"
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo&mode=MONTH&showPrint=0&showTabs=1&showDate=1&showNav=1&showCalendars=0&showTz=1&title=IASD%20Parada%20Inglesa&src=MmYwMTBjNjUyOTExNDgzZGJmZDQ0MmM3MzIxOTAzMTNmZDQ4YzcyNzcxMTRmNzFkNDIyYzEyNDFiODg0YTcwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AAhttps://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo&hl=pt_BR&showPrint=0&src=MmYwMTBjNjUyOTExNDgzZGJmZDQ0MmM3MzIxOTAzMTNmZDQ4YzcyNzcxMTRmNzFkNDIyYzEyNDFiODg0YTcwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA"
       style="border-width:0" 
       width="800" 
       height="600" 
@@ -19,14 +19,13 @@
   }
   .iframe-container{
     position: absolute;
-    padding: 5px;
     top:0;
     left:0;
-    height: 100vh;
-    width: 100vw;
+    height: 100%;
+    width: 100%;
   }
   iframe{
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
   }
 </style>
